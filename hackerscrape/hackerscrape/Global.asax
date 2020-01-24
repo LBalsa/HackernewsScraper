785a4f8f845d657e6508db02d83471115141802b
Luis Balsa <luis.balsa@gentrack.com>
2020-01-24 15:58:14 +0000

    Bare Projects

---
+++ hackerscrape/hackerscrape/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hackerscrape.WebApiApplication" Language="C#" %>
---
+++ hackerscrapecmd/hackerscrapecmd/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hackerscrapecmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://news.ycombinator.com/");
+
+            var headerNames = doc.DocumentNode.SelectNodes("//a[@class = 'storylink']").ToList();
+
+            foreach (var title in headerNames)
+            {
+                Console.WriteLine(title.InnerText);
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
